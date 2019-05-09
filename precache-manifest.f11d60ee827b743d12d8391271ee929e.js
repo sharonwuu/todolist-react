57b0f0e1522742661754fc00b32cc68f35a50bb0
Sharon <sharon@Sharon-MBP.local>
2019-05-09 19:21:04 +0800

    Updates

---
+++ precache-manifest.f11d60ee827b743d12d8391271ee929e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b992c7f725b44a818c2b012df2ed4a",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "5b8d4bf2a1a5cf0e1df6",
+    "url": "/todolist-react/static/css/main.1566de63.chunk.css"
+  },
+  {
+    "revision": "cefc7403c8beba75a044",
+    "url": "/todolist-react/static/js/2.1daa907d.chunk.js"
+  },
+  {
+    "revision": "5b8d4bf2a1a5cf0e1df6",
+    "url": "/todolist-react/static/js/main.862f2ddc.chunk.js"
+  },
+  {
+    "revision": "c6a1de0b206601e973bd",
+    "url": "/todolist-react/static/js/runtime~main.1492b700.js"
+  }
+]);
